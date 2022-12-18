8d0833bee1116590f81dbea928f7e072cf6e0aa0
Mami Nakano <85916526+mami-tus@users.noreply.github.com>
2022-12-18 23:38:33 +0900

    フォーマット修正

--- contest001_2/contest001_2.sql
+++ contest001_2/contest001_2.sql
@@ -8,12 +8,18 @@ SELECT
     SUM(STD.SP_TIME_9) AS "8時間以上9時間未満",
     SUM(STD.SP_TIME_9OVER) AS "9時間以上"
 FROM
-    SLEEP_TIME_DTL AS STD
-    INNER JOIN AGE_GRP AS AG ON AG.AGE_CODE = STD.AGE_CODE
-    INNER JOIN PREFECTURE AS PF ON STD.PF_CODE = PF.PF_CODE
+    SLEEP_TIME_DTL AS std
+INNER JOIN
+    AGE_GRP AS ag
+    ON
+        std.AGE_CODE = ag.AGE_CODE
+INNER JOIN
+    PREFECTURE AS pf
+    ON
+        std.PF_CODE = pf.PF_CODE
 WHERE
-    PF.PF_NAME IN ("北海道", "青森県", "岩手県", "宮城県", "福島県")
+    pf.PF_NAME IN ("北海道", "青森県", "岩手県", "宮城県", "福島県")
 GROUP BY
-    STD.AGE_CODE
+    std.AGE_CODE
 ORDER BY
-    STD.AGE_CODE;
+    std.AGE_CODE;
